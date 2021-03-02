f548344bc1cf9a55b66e6551ecd3b47ec27e2006
slavaMalenko <denouement20@yandex.ru>
2021-03-02 23:15:56 +0300

    continuation html and css, scrolling js

--- js/app.js
+++ js/app.js
@@ -27,6 +27,21 @@ document.querySelector('.portfolio_button').addEventListener('click', event => {
     })
 })
 
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
+
+
+
 
 
 
